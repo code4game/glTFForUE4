59b709277ed3e05ec677f9cd3a36151381dbe89b
Alex Chi <alexchi@c4g.io>
2017-09-13 01:44:32 +0800

    Import project

---
+++ Source/glTFForUE4/Private/glTFForUE4.cpp
@@ -0,0 +1,21 @@
+// Copyright 2017 Code 4 Game, Inc. All Rights Reserved.
+
+#include "glTFForUE4PrivatePCH.h"
+
+DEFINE_LOG_CATEGORY(LogglTFForUE4);
+
+#define LOCTEXT_NAMESPACE "FglTFForUE4Module"
+
+void FglTFForUE4Module::StartupModule()
+{
+	//
+}
+
+void FglTFForUE4Module::ShutdownModule()
+{
+	//
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FglTFForUE4Module, glTFForUE4)
---
+++ Source/glTFForUE4/Public/glTFForUE4PublicPCH.h
@@ -0,0 +1,7 @@
+// Copyright 2017 Code 4 Game, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Core.h"
+#include "CoreUObject.h"
+#include "Engine.h"
