d1995d13cf39c53e27d5b4463eeae8ba255ae904
Alex Chi <alexchi@c4g.io>
2018-03-31 00:17:00 +0800

    update extra

--- Extras/Batch/glTFForUE4Ed.Build.4.16.cs
+++ Extras/Batch/glTFForUE4Ed.Build.4.16.cs
@@ -42,7 +42,7 @@ public class glTFForUE4Ed : ModuleRules
 
         // libgltf
         {
-            string glTFPath = System.IO.Path.Combine(ExtraPathRoot, "libgltf_ue4", "0.1.3");
+            string glTFPath = System.IO.Path.Combine(ExtraPathRoot, "libgltf_ue4", "libgltf-0.1.3");
             string IncludePath = System.IO.Path.Combine(glTFPath, "include");
             string LibPath = "";
             string LibFilePath = "";
